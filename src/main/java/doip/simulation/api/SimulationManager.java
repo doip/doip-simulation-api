9c569b0f988102820cf2e6fbb05789a52baaa4f8
Marco Wehnert <marco.wehnert@gmx.de>
2023-12-08 03:50:51 +0000

    A few changes in the API

--- src/main/java/doip/simulation/api/SimulationManager.java
+++ src/main/java/doip/simulation/api/SimulationManager.java
@@ -2,13 +2,15 @@ package doip.simulation.api;
 
 import java.util.List;
 
+import doip.library.exception.DoipException;
+
 public interface SimulationManager {
 
-	public void start(String platform);
+	public void start(String platform) throws DoipException;
 	
-	public void start(String platform, String host);
+	// public void start(String platform, String host);
 	
-	public void stop();
+	public void stop(String platform);
 	
 	public Platform getPlatformByName(String name);
 	
